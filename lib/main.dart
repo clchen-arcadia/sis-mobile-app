daec64b5b7c8258d2cbb8b9abff2491bce6772ed
clchen-arcadia <clchen.arcadia@gmail.com>
2023-01-18 18:20:51 -0500

    Add more sections of the upcoming home page

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
+
     var mainArea = ColoredBox(
       color: colorScheme.surfaceVariant,
       child: AnimatedSwitcher(
@@ -82,31 +83,127 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Rithm SIS Mobile'),
-      actions: <Widget>[
-        IconButton(
-            icon: const Icon(Icons.home),
-            onPressed: () {},
-            tooltip: 'Show homepage'),
-        // IconButton(
-        //     icon: const Icon(Icons.dehaze_outlined),
-        //     onPressed: () {},
-        //     tooltip: 'Show homepage'),
-        DropdownButton(
-            value: dropdownValue,
-            icon: const Icon(Icons.dehaze_outlined),
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(value: value, child: Text(value));
-            }).toList(),
-            onChanged: (String? value) {
-              setState(() {
-                dropdownValue = value!;
-              });
-            })
-      ],
-    ));
-    //   body: LayoutBuilder(
+      appBar: AppBar(
+        title: const Text('Rithm SIS Mobile'),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {},
+              tooltip: 'Show homepage'),
+          // IconButton(
+          //     icon: const Icon(Icons.dehaze_outlined),
+          //     onPressed: () {},
+          //     tooltip: 'Show homepage'),
+          DropdownButton(
+              value: dropdownValue,
+              icon: const Icon(Icons.dehaze_outlined),
+              items: list.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                    value: value, child: Text(value));
+              }).toList(),
+              onChanged: (String? value) {
+                setState(() {
+                  dropdownValue = value!;
+                });
+              }),
+        ],
+      ),
+      body: mainArea,
+    );
+  }
+}
+
+class CohortDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 25),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(width: 25),
+              Text('Rithm 28 / Upcoming / breadcrumbs'),
+            ],
+          ),
+          SizedBox(width: 10),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(width: 25),
+              Text('Rithm 28 Upcoming Header'),
+            ],
+          ),
+          SizedBox(width: 10),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(width: 25),
+              ElevatedButton.icon(
+                onPressed: () {
+                  print("pressed button 1");
+                },
+                icon: Icon(Icons.calendar_month),
+                label: Text('Calendar'),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  print("pressed button 2");
+                },
+                icon: Icon(Icons.videocam),
+                label: Text('Video meeting'),
+              ),
+            ],
+          ),
+          SizedBox(width: 10),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(width: 25),
+              Text('Course: Sept - February / Course Info'),
+            ],
+          ),
+          SizedBox(width: 10),
+          DataTable(columns: const <DataColumn>[
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Date',
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Title',
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+            DataColumn(
+              label: Expanded(
+                child: Text(
+                  'Assets',
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+          ], rows: []),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+/// =======
+///     //   body: LayoutBuilder(
     //     builder: (context, constraints) {
     //       if (constraints.maxWidth < 450) {
     //         // Use a more mobile-friendly layout with BottomNavigationBar
@@ -169,34 +266,3 @@ class _MyHomePageState extends State<MyHomePage> {
     //   ),
     // );
     // }
-  }
-}
-
-class CohortDetailPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Rithm 28 Upcoming'),
-            ],
-          ),
-          SizedBox(width: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Course: Sept - February'),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
