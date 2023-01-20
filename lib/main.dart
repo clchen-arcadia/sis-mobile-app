800eb6a00b58e82fc6f5100f5f0e04a17c6ddb87
clchen-arcadia <clchen.arcadia@gmail.com>
2023-01-19 19:15:27 -0500

    Overhaul visual and change DataTable to a Column of Rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-// import 'dart:html';
-// import 'dart:ffi';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 // import 'package:intl/date_symbol_data_file.dart';
 import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'services/api_service.dart';
 import 'models/curriculum-items.dart';
 import 'models/cohort.dart';
@@ -15,15 +14,13 @@ Future main() async {
   runApp(MyApp());
 }
 
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
-
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-        padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.all(8),
         child: Row(
           children: [
             Expanded(
@@ -33,15 +30,30 @@ class MyApp extends StatelessWidget {
                     Container(
                       padding: const EdgeInsets.only(bottom: 8),
                       child: const Text(
-                        'Rithm 28 / Upcoming / Breadcrumbs',
+                        'Rithm 28 / Upcoming',
                         style: TextStyle(
                           color: Color.fromARGB(255, 160, 159, 159),
+                          fontSize: 18,
                         ),
                       ),
                     ),
-                    Text(
-                      'Rithm r28 Upcoming',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                    Row(
+                      children: [
+                        Text(
+                          'Rithm r28 ',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                          ),
+                        ),
+                        Text(
+                          'Upcoming',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 124, 122, 122),
+                            fontSize: 25,
+                          ),
+                        ),
+                      ],
                     ),
                   ]),
             )
@@ -75,12 +87,24 @@ class MyApp extends StatelessWidget {
       title: 'SIS Mobile App',
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 228, 107, 102)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 228, 107, 102),
+        ),
+        textTheme: GoogleFonts.sourceSerifProTextTheme(),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('{R} Rithm'),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.dehaze),
+              tooltip: "Menu",
+              onPressed: () {
+                print('Pressed button Menu');
+              },
+            )
+          ],
         ),
         body: ListView(
           children: [titleSection, buttonSection, CohortDetailPage()],
@@ -112,6 +136,7 @@ class _CohortDetailPageState extends State<CohortDetailPage> {
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         FutureBuilder<Cohort>(
           future: futureCohortData,
@@ -119,9 +144,10 @@ class _CohortDetailPageState extends State<CohortDetailPage> {
             if (snapshot.hasData) {
               return Container(
                   padding: const EdgeInsets.all(5),
-                  child: Text(
-                    'Course: ${snapshot.data!.id}',
-                  ));
+                  child: Text('Course: [Mon, Sep 26, 2022 - Fri, Feb 3, 2023]',
+                      style: TextStyle(
+                        fontSize: 18,
+                      )));
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
@@ -176,62 +202,161 @@ class DataClass extends StatelessWidget {
 
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
-      child: FittedBox(
-        child: DataTable(
-          sortColumnIndex: 1,
-          showCheckboxColumn: false,
-          // border: TableBorder.all(width: .5),
-          columns: const [
-            DataColumn(
-              label: Text(
-                "Date",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                "Title",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            DataColumn(
-              label: Text(
-                "Assets",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
-          rows: datalist
-              .where((data) => data.startDate.isAfter(startOfWeek))
-              .map((data) {
-            return DataRow(
-              cells: [
-                DataCell(
-                  Text(
-                    DateFormat('EEE, MM/dd h:mm a').format(data.startAt),
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.w500),
+      child: Column(
+        children: [
+              Row(children: [
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    "Date",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                DataCell(
-                  Text(
-                    data.title,
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.w500),
+                Expanded(
+                  flex: 8,
+                  child: Text(
+                    "Title",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                DataCell(
-                  Text(
-                    data.type.toString(),
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.w500),
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    "Assets",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ],
-            );
-          }).toList(),
-        ),
+              ]),
+              Divider(
+                height: 0,
+                thickness: 2,
+                color: Colors.black,
+              ),
+            ] +
+            datalist
+                .where((data) => data.startDate.isAfter(startOfWeek))
+                .map((data) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      DateFormat('EEE, MM/dd h:mm a').format(data.startAt),
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data.title,
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                        Text(
+                          "(${data.getTypeDisplayString()})",
+                          style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey),
+                        ),
+                        Text(
+                          data.description,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      "", // TODO: display the assets here as clickable icons
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Divider(
+                    height: 8,
+                    thickness: 5,
+                    color: Colors.grey,
+                  ),
+                ],
+              );
+            }).toList(),
       ),
     );
   }
 }
+
+
+// DataTable(
+//         dataRowHeight: 50,
+//         sortColumnIndex: 1,
+//         showCheckboxColumn: false,
+//         // border: TableBorder.all(width: .5),
+//         columns: const [
+//           DataColumn(
+//             label: Text(
+//               "Date",
+//               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//           DataColumn(
+//             label: Text(
+//               "Title",
+//               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//           DataColumn(
+//             label: Text(
+//               "Assets",
+//               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ],
+//         rows: datalist
+//             .where((data) => data.startDate.isAfter(startOfWeek))
+//             .map((data) {
+//           return DataRow(
+//             cells: [
+//               DataCell(
+//                 Text(
+//                   DateFormat('EEE, MM/dd h:mm a').format(data.startAt),
+//                   style: const TextStyle(
+//                       fontSize: 18, fontWeight: FontWeight.w500),
+//                 ),
+//               ),
+//               DataCell(
+//                 Text(
+//                   data.description,
+//                   style: const TextStyle(
+//                       fontSize: 18, fontWeight: FontWeight.w500),
+//                 ),
+//               ),
+//               DataCell(
+//                 Text(
+//                   data.type.toString(),
+//                   style: const TextStyle(
+//                       fontSize: 18, fontWeight: FontWeight.w500),
+//                 ),
+//               ),
+//             ],
+//           );
+//         }).toList(),
+//       ),
